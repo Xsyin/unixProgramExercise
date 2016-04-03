265e24b52cdc7d46bad81210ec88754184cfec42
Xsyin <943675853@qq.com>
2016-04-03 10:14:03 +0800

    change usage

--- 1-3/myls.c
+++ 1-3/myls.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 	DIR *dp;
 	struct dirent *dirp;
 	if(argc != 2)
-		err_quit("Usage: ls directory_name");
+		err_quit("Usage: ./myls directory_name");
 	if((dp = opendir(argv[1])) == NULL)
 		err_sys("Can't open %s",argv[1]);
 	while((dirp = readdir(dp)) != NULL)
